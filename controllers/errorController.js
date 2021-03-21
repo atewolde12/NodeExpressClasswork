9e3af854cb3364317b183bce3aa14709d63baf92
Aman Tewolde <aman.tewolde@ucdenver.edu>
2021-03-20 22:17:09 -0600

    organizing code

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,9 +1,11 @@
 const httpStatus= require("http-status-codes")
+
 exports.pageNotFoundError=(req,res)=>{
     let errorCode = httpStatus.NOT_FOUND;
     res.status(errorCode);
     res.render("error");
 }
+
 exports.interalServerError= (error,req,res,next) =>{
     let errorCode = httpStatus.INTERNAL_SERVER_ERROR;
     res.status(errorCode);
